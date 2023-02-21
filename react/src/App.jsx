5cda8e97beecdd7577b1d9ff19acc0c7c32634a7
Syota Nishimura <syota4723@gmail.com>
2023-02-21 22:11:46 +0900

    :lipstick: add: Install Heroicons

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { BeakerIcon } from '@heroicons/react/24/solid'
 
 function App() {
 
   return (
     <div className="text-purple-700">
         App.jsx
+        <BeakerIcon className='w-4' />
     </div>
   )
 }
