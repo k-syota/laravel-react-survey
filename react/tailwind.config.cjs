54b61504b5c2a6f451a8e4a2c9f792bab7b76151
Syota Nishimura <syota4723@gmail.com>
2023-02-22 21:31:44 +0900

    :sparkles: add: Create Login form

--- react/tailwind.config.cjs
+++ react/tailwind.config.cjs
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
       extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
 }
