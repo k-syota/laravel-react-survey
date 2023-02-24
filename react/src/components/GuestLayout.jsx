8deee755ec690fdea69ee4a4a7ebac5c407ee2ce
Syota Nishimura <syota4723@gmail.com>
2023-02-25 00:28:30 +0900

    :sparkles: add: Create Protected routes(GuestLayout)

--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -1,6 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function GuestLayout() {
+  const { currentUser, userToken } = useStateContext();
+
+  if (userToken) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <div>
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
