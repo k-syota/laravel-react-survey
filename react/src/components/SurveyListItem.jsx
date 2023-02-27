d4d796d667b6c5064e9a51005882ebf324ab9e89
Syota Nishimura <syota4723@gmail.com>
2023-02-27 23:34:16 +0900

    :sparkles: add: Survey Form 1

--- react/src/components/SurveyListItem.jsx
+++ react/src/components/SurveyListItem.jsx
@@ -1,5 +1,40 @@
+import {
+  ArrowTopRightOnSquareIcon,
+  PencilIcon,
+  TrashIcon,
+} from "@heroicons/react/24/outline";
 import React from "react";
+import TButton from "./core/TButton";
 
-export const SurveyListItem = ({ survey }) => {
-  return <div>SurveyListItem</div>;
+export const SurveyListItem = ({ survey, onDeleteClick }) => {
+  return (
+    <div className="flex flex-col py-4 shadow-md bg-white hover:bg-gray-50 h-[470px]">
+      <img
+        src={survey.image_url}
+        alt={survey.title}
+        className="w-full h-48 object-cover"
+      />
+      <h4 className="mt-4 text-lg font-bold">{survey.title}</h4>
+      <div
+        className="overflow-hidden flex-1"
+        dangerouslySetInnerHTML={{ __html: survey.description }}
+      ></div>
+      <div className="flex justify-between text-md-center mt-3">
+        <TButton to={`surveys/${survey.id}`}>
+          <PencilIcon className="w-5 h-5 mr-2" />
+          Edit
+        </TButton>
+        <div className="flex items-center">
+          <TButton href={`view/survey/${survey.slug}`} circle link>
+            <ArrowTopRightOnSquareIcon className="w-5 h-5" />
+          </TButton>
+          {survey.id && (
+            <TButton onClick={onDeleteClick} circle link color="red">
+              <TrashIcon className="w-5 h-5 mr-2" />
+            </TButton>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -4,13 +4,23 @@ import { useStateContext } from "../contexts/ContextProvider";
 
 export default function Surveys() {
   const { surveys } = useStateContext();
+
+  const onDeleteClick = () => {
+    console.log("On Delete");
+  };
+
   console.log(surveys);
+
   return (
     <>
       <PageComponent title="Surveys">
         <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
           {surveys.map((survey) => (
-            <SurveyListItem survey={survey} key={survey.id} />
+            <SurveyListItem
+              survey={survey}
+              key={survey.id}
+              onDeleteClick={onDeleteClick}
+            />
           ))}
         </div>
       </PageComponent>
