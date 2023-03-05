bfec8fdb69631b351a3fd42ffbd42129d716b60d
Syota Nishimura <syota4723@gmail.com>
2023-03-06 08:08:10 +0900

    :sparkles: add: Survey:index function

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -4,23 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\SurveyStoreRequest;
 use App\Http\Requests\SurveyUpdateRequest;
+use App\Http\Resources\SurveyResource;
+use App\Models\Survey;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class SurveyController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): Response
+    public function index(Request $request)
     {
-        //
+        $user = $request->user();
+        return SurveyResource::collection(
+            Survey::where('user_id', $user->id)
+            ->orderBy('created_at','desc')
+            ->paginate(10)
+        );
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(SurveyStoreRequest $request): Response
+    public function store(SurveyStoreRequest $request)
     {
         $user = $request->user();
 
@@ -30,7 +36,7 @@ class SurveyController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id): Response
+    public function show(string $id)
     {
         //
     }
@@ -38,7 +44,7 @@ class SurveyController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(SurveyUpdateRequest $request, string $id): Response
+    public function update(SurveyUpdateRequest $request, string $id)
     {
         //
     }
@@ -46,7 +52,7 @@ class SurveyController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id): Response
+    public function destroy(string $id)
     {
         //
     }
