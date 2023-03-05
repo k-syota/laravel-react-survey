857ec07ccc19e9ff1fe8f5e87f14b6918b7bb871
Syota Nishimura <syota4723@gmail.com>
2023-03-06 08:52:40 +0900

    :sparkles: add: private function saveImage

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -7,6 +7,8 @@ use App\Http\Requests\SurveyUpdateRequest;
 use App\Http\Resources\SurveyResource;
 use App\Models\Survey;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 
 class SurveyController extends Controller
 {
@@ -65,5 +67,42 @@ class SurveyController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+
+    /**
+     * Save image in local filesystem and return save image path
+     *
+     * @param [type] $image
+     * @throws \Exception
+     * @author   <email@email.com>
+     */
+    private function saveImage($image)
+    {
+        if (preg_match('/^data:image\/(\w+);base64,/', $image, $type)) {
+            $image = substr($image, strpos($image, ',') +1);
+            $type = strtolower($type[1]);
+            if(!in_array($type, ['jpg','jpeg','gif','png'])){
+                throw new \Exception('invalid image type');
+            }
+            $image = str_replace(' ','+',$image);
+            $image = base64_decode($image);
+
+            if($image === false){
+                throw new \Exception('base64_decode failed');
+            }
+        }else{
+            throw new \Exception('did not match image data URI with image data');
+        }
+
+        $dir = 'images/';
+        $file = Str::random(). ','. $type;
+        $absolutePath = public_path($dir);
+        $relativePath = $dir . $file;
+        if(!File::exists($absolutePath)){
+            File::makeDirectory($absolutePath,0755,true);
+        }
+        file_put_contents($relativePath, $image);
+
+        return $relativePath;
     }
 }
